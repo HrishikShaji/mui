cee3f535f850ed9e256c1aea5a0cf11faf6f4dfe
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-25 22:15:11 +0530

    started box

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
-	return <main></main>;
+  return (
+    <main>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          bgcolor: "gray",
+          color: "blue",
+        }}
+      >
+        <Typography>HELLO</Typography>
+      </Box>
+    </main>
+  );
 }
